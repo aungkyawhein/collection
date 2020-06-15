c50e8521080795c60a07b2f300e367a1101e6f0f
aungkyawhein <akh.infotech@gmail.com>
2020-06-15 22:52:04 +0800

    firebase api keys same for both dev and prod for now

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-
+    apiKey: 'AIzaSyCElmUqrJXyhF3TvDi_Ou7R-5zZAl4JmUE',
+    authDomain: 'collection-b955a.firebaseapp.com',
+    databaseURL: 'https://collection-b955a.firebaseio.com',
+    projectId: 'collection-b955a',
+    storageBucket: 'collection-b955a.appspot.com',
+    messagingSenderId: '818834817576',
+    appId: '1:818834817576:web:c0a4251b8710fd4258eb1d',
+    measurementId: 'G-VLXLW1K41Z'
   }
 };
 
