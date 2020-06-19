595b11074ffe65236ab55e37edcbc3ca0f70dbe5
aungkyawhein <akh.infotech@gmail.com>
2020-06-19 23:29:59 +0800

    firebaes api keys placeholders

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCElmUqrJXyhF3TvDi_Ou7R-5zZAl4JmUE',
-    authDomain: 'collection-b955a.firebaseapp.com',
-    databaseURL: 'https://collection-b955a.firebaseio.com',
-    projectId: 'collection-b955a',
-    storageBucket: 'collection-b955a.appspot.com',
-    messagingSenderId: '818834817576',
-    appId: '1:818834817576:web:c0a4251b8710fd4258eb1d',
-    measurementId: 'G-VLXLW1K41Z'
+    apiKey: 'FIREBASE_API_KEY',
+    authDomain: 'FIREBASE_PROJECT_ID.firebaseapp.com',
+    databaseURL: 'https://FIREBASE_PROJECT_ID.firebaseio.com',
+    projectId: 'FIREBASE_PROJECT_ID',
+    storageBucket: 'FIREBASE_PROJECT_ID.appspot.com',
+    messagingSenderId: 'MESSAGING_SENDER_ID',
+    appId: 'APPID',
+    measurementId: 'MEASUREMENT_ID'
   }
 };
 
