f7dcd7bd2361a7e5ce5c0f7e53c0e88dfae146b6
aungkyawhein <akh.infotech@gmail.com>
2020-07-07 22:41:42 +0800

    category service split

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { CollectionsService } from '../services/collections.service';
+import { CategoryService } from '../services/category.service';
 
 import { Item } from '../models/item';
 import { Category } from '../models/category';
@@ -14,20 +14,22 @@ import { Category } from '../models/category';
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
-
-  private categoriesCollection: AngularFirestoreCollection<Category>;
   items: Observable<Item[]>;
   filteredItems: Observable<Item[]>;
   categories: Observable<Category[]>;
 
-  constructor(firestore: AngularFirestore, private collectionService: CollectionsService) {
-    this.categoriesCollection = firestore.collection<Category>('categories');
-    this.categories = this.categoriesCollection.valueChanges();
+  constructor(
+    private collectionService: CollectionsService,
+    private categoryService: CategoryService
+  ) {
+
   }
 
   ngOnInit(): void {
     this.items = this.collectionService.loadAllItems();
     this.filteredItems = this.getFilteredItems(null);
+
+    this.categories = this.categoryService.loadAllItems();
   }
 
   getFilteredItems(category: Category) {
